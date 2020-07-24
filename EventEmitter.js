a36caa4b666bdbdc8984917b0951ed2283e42251
ling.yu <ling.yu@alo7.com>
2020-07-24 17:18:55 +0800

    off once

--- EventEmitter.js
+++ EventEmitter.js
@@ -10,7 +10,6 @@ EventEmitter.prototype.on = function (eventName, callback) {
   this._events[eventName] = callbacks
 }
 EventEmitter.prototype.emit = function (eventName, ...args) {
-  console.log(this._events)
   let callbacks = this._events[eventName]
   if (!callbacks) {
     return
@@ -19,10 +18,29 @@ EventEmitter.prototype.emit = function (eventName, ...args) {
     fn(...args)
   });
 }
+EventEmitter.prototype.off = function (eventName, callback) {
+  if (!this._events[eventName]){
+    return
+  }
+  this._events = this._events[eventName].filter(event => {
+    return event !== callback && event.r !== callback
+  })
+}
+EventEmitter.prototype.once = function (eventName, callback) {
+  const one =  () => {
+    callback()
+    this.off(eventName, one)
+  }
+  one.r = callback
+  this.on(eventName, one)
+}
 
-// const girl = new EventEmitter()
-// girl.emit('cry', 111)
-// girl.on('cry', (q) => {
-//   console.log('crying', q)
-// })
+const a = () => {
+  console.log('a')
+}
+const girl = new EventEmitter()
+girl.on('cry', a)
+girl.off('cry', a)
+girl.emit('cry', 111)
+
 
